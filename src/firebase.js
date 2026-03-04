6b458266328c5428ae93296483636f2d9788e9cc
RI88Z <01187268@pw.edu.pl>
2026-03-04 19:13:44 +0100

    Code smells

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9PEW5Ka18VMkNHFZqLhdUnI7SLnXrTlA",
-  authDomain: "habit-tracker-184bd.firebaseapp.com",
-  projectId: "habit-tracker-184bd",
-  storageBucket: "habit-tracker-184bd.firebasestorage.app",
-  messagingSenderId: "533279044254",
-  appId: "1:533279044254:web:b27f84dbaff378e534818f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING,
+  appId: import.meta.env.VITE_FIREBASE_API_KEY
 };
 
 const app = initializeApp(firebaseConfig);
